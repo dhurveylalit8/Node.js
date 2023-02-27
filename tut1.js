25aa8e11dfea967eed0a6779c8e714b602e16ca2
LalitDhurvey <dhurvelalit8@gmail.com>
2023-02-27 10:06:29 +0530

    initial commit

---
+++ tut1.js
@@ -0,0 +1,7 @@
+// const fs = require("fs")
+// fs.writeFileSync("file3.txt", "Hellow programmers")
+
+// ===================
+
+const fs1 = require("fs").writeFileSync;
+fs1("file4.txt", "just testing another method")
